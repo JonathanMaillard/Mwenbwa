e19d67d01389cd10f4078320e90c3eb21eeef8e5
Bertrand2 <dossogne.bertrand@gmail.com>
2021-02-11 17:18:13 +0100

    [WIP] trying to figure out how requests work

--- src/client/app.js
+++ src/client/app.js
@@ -8,7 +8,24 @@
 
 import * as React from "react";
 import ReactDOM from "react-dom";
+const axios = require("axios");
 
 import HelloWorld from "./components/hello";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
+const test = () => {
+    const id = 5;
+    axios
+        .post("/test", {content: 5})
+        .then(() => {
+            console.log("yay");
+        })
+        .catch(e => {
+            console.log("sad because :", e);
+        });
+    // var xmlHttp = new XMLHttpRequest();
+    // xmlHttp.open( "POST", `/`, false ); // false for synchronous request
+    // xmlHttp.send();
+    // console.log( xmlHttp.responseText );
+};
+
+ReactDOM.render(<HelloWorld onClick={test} />, document.querySelector("#app"));
