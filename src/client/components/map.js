97466664b71655ca72f91efd8e7efb5ff685ab7c
Tanya Leenders <tanya.leenders@gmail.com>
2021-02-18 14:38:34 +0100

    tried to chage default marker

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,10 +1,39 @@
 import * as React from "react";
-import {MapContainer, TileLayer, Marker} from "react-leaflet";
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import {divIcon} from "leaflet";
+import ReactDOMServer from "react-dom/server";
+import MySVG from "../../ressources/images/cancel.svg";
 
-const Map = () => (
-    <MapContainer center={[50.62571, 5.56878]} zoom={15}>
-        <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-    </MapContainer>
-);
+const Map = () => {
+    // const treesIcon = () => {
+    //     divIcon(
+    //         {
+    //             html: <img src="../../ressources/images/cancel.svg" alt=""/>
+    //         }
+    //     )
+    // }
+    // const iconMarkup = ReactDOMServer.renderToString(<svg />);
+    const customMarkerIcon = divIcon({
+        html: ReactDOMServer.renderToString(
+            <div className={"icon"}>
+                <MySVG />
+            </div>,
+        ),
+    });
+
+    return (
+        <MapContainer center={[50.62571, 5.56878]} zoom={15}>
+            <TileLayer
+                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+            />
+
+            <Marker position={[50.62571, 5.56878]} icon={customMarkerIcon}>
+                <Popup>
+                    {"A pretty CSS3 popup."} <br /> {"Easily customizable."}
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
 
 export default Map;
