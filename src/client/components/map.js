8010311f5f09a5aeffc6c8bf94e83de96c6a2d3f
Tanya Leenders <tanya.leenders@gmail.com>
2021-02-19 16:53:33 +0100

    responsive for rules & disconnect

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import {divIcon} from "leaflet";
-import ReactDOMServer from "react-dom/server";
-import MySVG from "../../ressources/images/cancel.svg";
+// import {divIcon} from "leaflet";
+// import ReactDOMServer from "react-dom/server";
+// import MySVG from "../../ressources/images/cancel.svg";
 
-const Map = () => {
+const Map = () => (
     // const treesIcon = () => {
     //     divIcon(
     //         {
@@ -13,27 +13,22 @@ const Map = () => {
     //     )
     // }
     // const iconMarkup = ReactDOMServer.renderToString(<svg />);
-    const customMarkerIcon = divIcon({
-        html: ReactDOMServer.renderToString(
-            <div className={"icon"}>
-                <MySVG />
-            </div>,
-        ),
-    });
+    // const customMarkerIcon = divIcon({
+    //     html: ReactDOMServer.renderToString(
+    //         <div className={"icon"}>
+    //             <MySVG />
+    //         </div>,
+    //     ),
+    // });
 
-    return (
-        <MapContainer center={[50.62571, 5.56878]} zoom={15}>
-            <TileLayer
-                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-            />
+    <MapContainer center={[50.62571, 5.56878]} zoom={15}>
+        <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
 
-            <Marker position={[50.62571, 5.56878]} icon={customMarkerIcon}>
-                <Popup>
-                    {"A pretty CSS3 popup."} <br /> {"Easily customizable."}
-                </Popup>
-            </Marker>
-        </MapContainer>
-    );
-};
-
+        <Marker position={[50.62571, 5.56878]}>
+            <Popup>
+                {"A pretty CSS3 popup."} <br /> {"Easily customizable."}
+            </Popup>
+        </Marker>
+    </MapContainer>
+);
 export default Map;
