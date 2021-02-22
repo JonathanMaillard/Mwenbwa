e10d70a1cbe844763673113380caf6ef7e7075d4
Tanya Leenders <tanya.leenders@gmail.com>
2021-02-22 15:54:01 +0100

    added component Signin/up, display none temporary

--- src/client/components/sign.js
+++ src/client/components/sign.js
@@ -1,44 +1,105 @@
 import * as React from "react";
+import SignSVG from "../../ressources/images/undraw_nature.svg";
+import {showSignIn, showSignUp} from "../display/show-sign-form";
+import {hideSignForm} from "../display/hide-sign-form";
 
 const Sign = () => (
     <div className={"sign"}>
         <div className={"sign__content"}>
             <div className={"sign__content__title"}>
-                <button className={"sign-up-btn"} type={"button"}>
+                <button
+                    className={"sign-up-btn sign-btn-checked"}
+                    type={"button"}
+                    onClick={showSignUp}>
                     {"Sign Up"}
                 </button>
-                <button className={"sign-in-btn"} type={"button"}>
+                <button
+                    className={"sign-in-btn"}
+                    type={"button"}
+                    onClick={showSignIn}>
                     {"Sign In"}
                 </button>
             </div>
 
-            <div className={"sign__content__sign-up"}>
-                <form action={"????????"}>
-                    <label htmlFor={"userimg"}>{"Profile picture:"}</label>
-                    <br />
+            <div className={"sign-up sign-active"}>
+                <img className={"sign-up__img"} src={SignSVG} alt={"Sign up"} />
+                <form action={"????????"} className={"sign-up__form"}>
+                    <label htmlFor={"userimg"} className={"sign-up__form__img"}>
+                        {"Profile picture:"}
+                        <div className={"preview"}>
+                            <span className={"choose-txt"}>
+                                {"Choose a file"}
+                            </span>
+                        </div>
+                        <input
+                            className={"file-input"}
+                            type={"file"}
+                            accept={"image/*"}
+                            name={"userimg"}
+                        />
+                    </label>
+
+                    <div className={"sign-up__form__name"}>
+                        <label htmlFor={"username"} className={"label"}>
+                            {"Username:"}
+                        </label>
+                        <input type={"text"} name={"username"} />
+                    </div>
+
+                    <div className={"sign-up__form__email"}>
+                        <label htmlFor={"useremail"} className={"label"}>
+                            {"Email adress:"}
+                        </label>
+                        <input type={"email"} name={"useremail"} />
+                    </div>
+
+                    <div className={"sign-up__form__pwd"}>
+                        <label htmlFor={"usepwd"} className={"label"}>
+                            {"Password:"}
+                        </label>
+                        <input type={"password"} name={"userpwd"} />
+                    </div>
+
+                    <div className={"sign-up__form__color"}>
+                        <label htmlFor={"colortheme"} className={"label"}>
+                            {"Color theme:"}
+                        </label>
+                    </div>
+
                     <input
-                        type={"file"}
-                        accept={"image/*"}
-                        id={"userimg"}
-                        name={"userimg"}
+                        className={"sign-up__form__btn"}
+                        type={"submit"}
+                        value={"Sign Up"}
+                        onClick={hideSignForm}
                     />
-                    <br />
-
-                    <label htmlFor={"username"}>{"Username:"}</label>
-                    <br />
-                    <input type={"text"} id={"username"} name={"username"} />
-                    <br />
-
-                    <label htmlFor={"useremail"}>{"Email adress:"}</label>
-                    <br />
-                    <input type={"email"} id={"useremail"} name={"useremail"} />
-                    <br />
-
-                    <label htmlFor={"colortheme"}>{"Color theme:"}</label>
-                    <br />
                 </form>
             </div>
-            <div className={"sign__content__sign-in"} />
+
+            <div className={"sign-in"}>
+                <img className={"sign-in__img"} src={SignSVG} alt={"Sign up"} />
+                <form action={"????????"} className={"sign-in__form"}>
+                    <div className={"sign-in__form__name"}>
+                        <label htmlFor={"username"} className={"label"}>
+                            {"Username:"}
+                        </label>
+                        <input type={"text"} name={"username"} />
+                    </div>
+
+                    <div className={"sign-in__form__pwd"}>
+                        <label htmlFor={"usepwd"} className={"label"}>
+                            {"Password:"}
+                        </label>
+                        <input type={"password"} name={"userpwd"} />
+                    </div>
+
+                    <input
+                        className={"sign-in__form__btn"}
+                        type={"submit"}
+                        value={"Sign In"}
+                        onClick={hideSignForm}
+                    />
+                </form>
+            </div>
         </div>
     </div>
 );
