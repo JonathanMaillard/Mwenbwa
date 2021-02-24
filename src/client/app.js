39e6d9883e9fd1b8186ed774c7aac42185600c93
Tanya Leenders <tanya.leenders@gmail.com>
2021-02-24 11:53:36 +0100

    managed axios requests

--- src/client/app.js
+++ src/client/app.js
@@ -15,8 +15,6 @@ import Map from "./components/map";
 import Sign from "./components/sign";
 import Rules from "./components/rules";
 import Disconnect from "./components/disconnect";
-import Leaderboard from "./components/leaderboard";
-import Gamelog from "./components/gamelog";
 import Profile from "./components/profile";
 import Button from "./components/button";
 import Dashboard from "./components/dashboard";
@@ -38,35 +36,9 @@ const App = () => (
         <Sign />
         <Rules />
         <Disconnect />
-        <Gamelog />
-
         <Dashboard />
     </div>
 );
-
-axios
-    .get(`/leaderboard`)
-    .then(response => {
-        ReactDOM.render(
-            <Leaderboard leaderboard={response.data} />,
-            document.querySelector("#leaderboard"),
-        );
-    })
-    .catch(e => {
-        console.log("sad because :", e);
-    });
-
-axios
-    .get(`/logs`)
-    .then(response => {
-        ReactDOM.render(
-            <Gamelog logs={response.data} />,
-            document.querySelector("#gamelog"),
-        );
-    })
-    .catch(e => {
-        console.log("sad because :", e);
-    });
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
