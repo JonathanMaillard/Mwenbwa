f06cc3d87e2b36c0e91a3c6e072637aa9d3aa985
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-02-25 16:23:59 +0100

    change lockedBy to owner

--- src/server/db-calls.js
+++ src/server/db-calls.js
@@ -290,8 +290,7 @@ const dbRegister = (userName, userPassword, userEmail, userColor) => {
 const dbBuyTree = (tree, userId, treePrice) => {
     const client = new MongoClient(uri, {useUnifiedTopology: true});
 
-
-    console.log(tree, userId, treePrice );
+    console.log(tree, userId, treePrice);
 
     async function run() {
         try {
@@ -372,7 +371,7 @@ const dbLockTree = (tree, userId, treeLockPrice) => {
             const updateDoc = {
                 $set: {
                     locked: true,
-                    lockedBy: user,
+                    owner: user,
                 },
             };
 
